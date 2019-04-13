55e9c2169be08ec602ca336cfc35276923d073a1
Yorick de Wid <yorick17@outlook.com>
2019-04-13 11:17:22 +0200

    Make the core more readable

--- chat_server.c
+++ chat_server.c
@@ -3,8 +3,7 @@
  *
  * Author:      Yorick de Wid
  * Description: Simple chatroom in C
- * Version:     1.0
- *
+ * Version:     1.4
  */
 
 #include <sys/socket.h>
@@ -37,8 +36,8 @@ client_t *clients[MAX_CLIENTS];
 /* Add client to queue */
 void queue_add(client_t *cl){
     int i;
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(!clients[i]){
+    for (i = 0; i < MAX_CLIENTS; ++i) {
+        if (!clients[i]) {
             clients[i] = cl;
             return;
         }
@@ -48,9 +47,9 @@ void queue_add(client_t *cl){
 /* Delete client from queue */
 void queue_delete(int uid){
     int i;
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(clients[i]){
-            if(clients[i]->uid == uid){
+    for (i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i]) {
+            if (clients[i]->uid == uid) {
                 clients[i] = NULL;
                 return;
             }
@@ -61,10 +60,10 @@ void queue_delete(int uid){
 /* Send message to all clients but the sender */
 void send_message(char *s, int uid){
     int i;
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(clients[i]){
-            if(clients[i]->uid != uid){
-                if(write(clients[i]->connfd, s, strlen(s))<0){
+    for (i = 0; i < MAX_CLIENTS; ++i) {
+        if (clients[i]) {
+            if (clients[i]->uid != uid) {
+                if (write(clients[i]->connfd, s, strlen(s)) < 0) {
                     perror("Write to descriptor failed");
                     exit(-1);
                 }
@@ -76,9 +75,9 @@ void send_message(char *s, int uid){
 /* Send message to all clients */
 void send_message_all(char *s){
     int i;
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(clients[i]){
-            if(write(clients[i]->connfd, s, strlen(s))<0){
+    for (i = 0; i <MAX_CLIENTS; ++i){
+        if (clients[i]) {
+            if (write(clients[i]->connfd, s, strlen(s)) < 0) {
                 perror("Write to descriptor failed");
                 exit(-1);
             }
@@ -88,7 +87,7 @@ void send_message_all(char *s){
 
 /* Send message to sender */
 void send_message_self(const char *s, int connfd){
-    if(write(connfd, s, strlen(s))<0){
+    if (write(connfd, s, strlen(s)) < 0) {
         perror("Write to descriptor failed");
         exit(-1);
     }
@@ -97,10 +96,10 @@ void send_message_self(const char *s, int connfd){
 /* Send message to client */
 void send_message_client(char *s, int uid){
     int i;
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(clients[i]){
-            if(clients[i]->uid == uid){
-                if(write(clients[i]->connfd, s, strlen(s))<0){
+    for (i = 0; i < MAX_CLIENTS; ++i){
+        if (clients[i]) { 
+            if (clients[i]->uid == uid) {
+                if (write(clients[i]->connfd, s, strlen(s))<0) {
                     perror("Write to descriptor failed");
                     exit(-1);
                 }
@@ -113,8 +112,8 @@ void send_message_client(char *s, int uid){
 void send_active_clients(int connfd){
     int i;
     char s[64];
-    for(i=0;i<MAX_CLIENTS;i++){
-        if(clients[i]){
+    for (i = 0; i < MAX_CLIENTS; ++i){
+        if (clients[i]) {
             sprintf(s, "<< [%d] %s\r\n", clients[i]->uid, clients[i]->name);
             send_message_self(s, connfd);
         }
@@ -123,8 +122,8 @@ void send_active_clients(int connfd){
 
 /* Strip CRLF */
 void strip_newline(char *s){
-    while(*s != '\0'){
-        if(*s == '\r' || *s == '\n'){
+    while (*s != '\0') {
+        if (*s == '\r' || *s == '\n') {
             *s = '\0';
         }
         s++;
@@ -159,60 +158,60 @@ void *handle_client(void *arg){
     send_message_self("<< see /help for assistance\r\n", cli->connfd);
 
     /* Receive input from client */
-    while((rlen = read(cli->connfd, buff_in, sizeof(buff_in)-1)) > 0){
-            buff_in[rlen] = '\0';
-            buff_out[0] = '\0';
+    while ((rlen = read(cli->connfd, buff_in, sizeof(buff_in)-1)) > 0) {
+        buff_in[rlen] = '\0';
+        buff_out[0] = '\0';
         strip_newline(buff_in);
 
         /* Ignore empty buffer */
-        if(!strlen(buff_in)){
+        if (!strlen(buff_in)) {
             continue;
         }
 
         /* Special options */
-        if(buff_in[0] == '/'){
+        if (buff_in[0] == '/') {
             char *command, *param;
             command = strtok(buff_in," ");
-            if(!strcmp(command, "/quit")){
+            if (!strcmp(command, "/quit")) {
                 break;
-            }else if(!strcmp(command, "/ping")){
+            } else if (!strcmp(command, "/ping")) {
                 send_message_self("<< pong\r\n", cli->connfd);
-            }else if(!strcmp(command, "/nick")){
+            } else if (!strcmp(command, "/nick")) {
                 param = strtok(NULL, " ");
-                if(param){
+                if (param) {
                     char *old_name = strdup(cli->name);
                     strcpy(cli->name, param);
                     sprintf(buff_out, "<< %s is now known as %s\r\n", old_name, cli->name);
                     free(old_name);
                     send_message_all(buff_out);
-                }else{
+                } else {
                     send_message_self("<< name cannot be null\r\n", cli->connfd);
                 }
-            }else if(!strcmp(command, "/msg")){
+            } else if (!strcmp(command, "/msg")) {
                 param = strtok(NULL, " ");
-                if(param){
+                if (param) {
                     int uid = atoi(param);
                     param = strtok(NULL, " ");
-                    if(param){
+                    if (param) {
                         sprintf(buff_out, "[PM][%s]", cli->name);
-                        while(param != NULL){
+                        while (param != NULL) {
                             strcat(buff_out, " ");
                             strcat(buff_out, param);
                             param = strtok(NULL, " ");
                         }
                         strcat(buff_out, "\r\n");
                         send_message_client(buff_out, uid);
-                    }else{
+                    } else {
                         send_message_self("<< message cannot be null\r\n", cli->connfd);
                     }
-                }else{
+                } else {
                     send_message_self("<< reference cannot be null\r\n", cli->connfd);
                 }
-            }else if(!strcmp(command, "/list")){
+            } else if(!strcmp(command, "/list")) {
                 sprintf(buff_out, "<< clients %d\r\n", cli_count);
                 send_message_self(buff_out, cli->connfd);
                 send_active_clients(cli->connfd);
-            }else if(!strcmp(command, "/help")){
+            } else if (!strcmp(command, "/help")) {
                 strcat(buff_out, "<< /quit     Quit chatroom\r\n");
                 strcat(buff_out, "<< /ping     Server test\r\n");
                 strcat(buff_out, "<< /nick     <name> Change nickname\r\n");
@@ -220,10 +219,10 @@ void *handle_client(void *arg){
                 strcat(buff_out, "<< /list     Show active clients\r\n");
                 strcat(buff_out, "<< /help     Show help\r\n");
                 send_message_self(buff_out, cli->connfd);
-            }else{
+            } else {
                 send_message_self("<< unknown command\r\n", cli->connfd);
             }
-        }else{
+        } else {
             /* Send message */
             snprintf(buff_out, sizeof(buff_out), "[%s] %s\r\n", cli->name, buff_in);
             send_message(buff_out, cli->uid);
@@ -263,13 +262,13 @@ int main(int argc, char *argv[]){
     signal(SIGPIPE, SIG_IGN);
 
     /* Bind */
-    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+    if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Socket binding failed");
         return 1;
     }
 
     /* Listen */
-    if(listen(listenfd, 10) < 0){
+    if (listen(listenfd, 10) < 0) {
         perror("Socket listening failed");
         return 1;
     }
@@ -277,12 +276,12 @@ int main(int argc, char *argv[]){
     printf("<[ SERVER STARTED ]>\n");
 
     /* Accept clients */
-    while(1){
+    while (1) {
         socklen_t clilen = sizeof(cli_addr);
         connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
         /* Check if max clients is reached */
-        if((cli_count+1) == MAX_CLIENTS){
+        if ((cli_count + 1) == MAX_CLIENTS) {
             printf("<< max clients reached\n");
             printf("<< reject ");
             print_client_addr(cli_addr);
